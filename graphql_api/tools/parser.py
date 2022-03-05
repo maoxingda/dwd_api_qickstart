bbff5cc86d6a2fbb4109535d903312c26ee9a58f
maoxd <maoxingda@meican.com>
2022-03-05 19:54:46 +0800

    bak

--- graphql_api/tools/parser.py
+++ graphql_api/tools/parser.py
@@ -1,12 +1,7 @@
 """Parse tables and select columns list from graphql AST"""
 
 
-def parse(ast, tables):
-    def get_table_alias(table_name):
-        for table in tables:
-            if table_name == table.name:
-                return table.alias
-
+def parse(ast, tables_alias):
     def _parse(node, tables, table_name, level):
         if level > 2:
             tables[node.name.value] = {
@@ -18,7 +13,7 @@ def parse(ast, tables):
             if level == 2 and field.name.value in ('sql', 'data'):
                 continue
             if field.selection_set is None:
-                tables[node.name.value]['columns'].append(f'{get_table_alias(node.name.value)}.{field.name.value}')
+                tables[node.name.value]['columns'].append(f'{tables_alias[node.name.value]}.{field.name.value}')
                 continue
 
             _parse(field, tables, node.name.value, level + 1)
