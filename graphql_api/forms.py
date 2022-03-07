b48730d88314218756215424ccb57ae3e592f8f6
maoxd <maoxingda@meican.com>
2022-03-07 17:03:29 +0800

    bak

--- graphql_api/forms.py
+++ graphql_api/forms.py
@@ -16,8 +16,7 @@ class RelationshipAdminForm(ModelForm):
         ltbl = self.cleaned_data['left_table_name']
         rtbl = self.cleaned_data['right_table_name']
 
-        if 1 or len(self.changed_data) == 0:
-            # TODO: 新增需要校验、改变不需要。如何区分
+        if not self.instance.id:
             if ltbl.id == rtbl.id:
                 raise ValidationError(f'{self.cleaned_data["left_table_name"]} can not join with it self.')
 
@@ -25,13 +24,13 @@ class RelationshipAdminForm(ModelForm):
             if ltr.count() > 0:
                 raise ValidationError(format_html(
                     f'<a href=http://127.0.0.1:8000/admin/graphql_api/relationship/{ltr[0].id}/change/>'
-                    f'({ltbl.name} <---> {rtbl.name}), already exist.</a>'))
+                    f'({ltbl.name} ---> {rtbl.name}), already exist.</a>'))
 
             rtr = Relationship.objects.filter(right_table_name__id=ltbl.id).filter(left_table_name__id=rtbl.id)
             if rtr.count() > 0:
                 raise ValidationError(format_html(
                     f'<a href=http://127.0.0.1:8000/admin/graphql_api/relationship/{rtr[0].id}/change/>'
-                    f'({rtbl.name} <---> {ltbl.name}), already exist.</a>'))
+                    f'({rtbl.name} ---> {ltbl.name}), already exist.</a>'))
 
         # is column name exist?
         columns = re.findall(r'\{\{ \w \}\}\.\w+', self.cleaned_data['join_condition'])
